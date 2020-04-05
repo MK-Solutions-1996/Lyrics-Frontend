bba41b531eec962ba2dad637959481a18f880efe
Kalana-Ramesh <58284255+Kalana-Ramesh@users.noreply.github.com>
2020-04-06 00:49:13 +0530

    Create TestData.js

---
+++ src/TestData.js
@@ -0,0 +1,145 @@
+export const test_artist_array = [
+  {
+    image: {
+      imagePath: 'images\\Lahiru Perera_Lahiru.jpg',
+      image:
+        'https://fmderana.lk/image/data/Artists/1438589513lahiru-430-430.jpg',
+      imageAvailability: true,
+    },
+    _id: '5e89b84769be1558f04ed06d',
+    sinhalaName: 'ලහිරු පෙරේරා',
+    singlishName: 'Lahiru Perera',
+    period: 'Old',
+  },
+  {
+    image: {
+      imagePath: 'images\\Sanuka Wikramasingha_Sanuka.jpg',
+      image:
+        'https://song.sgp1.cdn.digitaloceanspaces.com/image/artist/2019/03/534.jpg',
+      imageAvailability: true,
+    },
+    _id: '5e89b87769be1558f04ed06e',
+    sinhalaName: 'සනුක වික්‍රමසිංහ',
+    singlishName: 'Sanuka Wikramasingha',
+    period: 'New',
+  },
+  {
+    image: {
+      imagePath: 'images\\Umaria Sinhawansa_Umaria.jpg',
+      image:
+        'https://i0.wp.com/www.myevents.lk/wp-content/uploads/2016/06/Umaria.jpg?fit=272%2C411',
+      imageAvailability: true,
+    },
+    _id: '5e89b8df69be1558f04ed06f',
+    sinhalaName: 'උමාරියා සින්හවංස',
+    singlishName: 'Umaria Sinhawansa',
+    period: 'New',
+  },
+  {
+    image: {
+      imagePath: 'default',
+      image: 'default',
+      imageAvailability: false,
+    },
+    _id: '5e89b94169be1558f04ed070',
+    sinhalaName: 'අමරසිරි පීරිස්',
+    singlishName: 'Amarasiri Peiris',
+    period: 'Old',
+  },
+  {
+    image: {
+      imagePath: 'images\\Bathiya n Santhush_bathiya-santhush_f.jpg',
+      image: 'https://cbr.lk/wp-content/uploads/2015/09/BNS.jpg',
+      imageAvailability: true,
+    },
+    _id: '5e89bac869be1558f04ed072',
+    sinhalaName: 'භාතිය සන්තුෂ්',
+    singlishName: 'Bathiya n Santhush',
+    period: 'New',
+  },
+  {
+    image: {
+      imagePath: 'images\\Ruwan Hettiarachchi_Ruwan.jpg',
+      image:
+        'https://www.fmderana.lk/image/data/1480403079Amathumak_Ruwan_Hettiarachchi1.jpg',
+      imageAvailability: true,
+    },
+    _id: '5e89bb4e69be1558f04ed073',
+    sinhalaName: 'රුවන් හෙට්ටිආරච්චි',
+    singlishName: 'Ruwan Hettiarachchi',
+    period: 'New',
+  },
+];
+
+export const test_song_array = [
+  {
+    audio: {
+      audioPath: 'audios\\Sansara Sihine_Seekers_Sansara_Sihinaye.mp3',
+      audio:
+        'http://localhost:4000/Sansara Sihine_Seekers_Sansara_Sihinaye.mp3',
+      audioAvailability: true,
+    },
+    categories: ['Love'],
+    likes: 0,
+    artist: ['5e89b87769be1558f04ed06e'],
+    _id: '5e89b9de69be1558f04ed071',
+    sinhalaTitle: 'සංසාර සිහිනේ',
+    singlishTitle: 'Sansara Sihine',
+    song:
+      'සංසාර සිහිනේ පෙර මග මා\nසිටියා\nකවදාද නුඹ එන්නේ\nආදර දේදුන්නේ\nඅඩවන් නෙතු විහිදා\nමා නුඹේ කුමරා\nසංසාර සිහිනයේ\nමා නුඹේ කුමරා\nමා නුඹේ කුමරා\n\nපියමැන එන්න\nමේ සිහිනේ සිරකාරිය\nවන්න\nතුරුලට එන්න\nපවසන් නුඹ මගෙමයි කියා\nකුමරී\nපියමැන එන්න\nමගේ සිහිනේ සිරකාරිය\nවන්න\nතුරුලට එන්න\nපවසන් නුඹ මගෙමයි කියා\nසංසාර සිහිනයේ\nමා නුඹේ කුමරා\nමා නුඹේ කුමරා\nමා නුඹේ කුමරා\nමා නුඹේ කුමරා\n\nසිහින දකින්න\nමේ සිහිනේ හිමිකාරිය\nවන්න\nදිවියට එන්න\nපවසන් නුඹ මගෙමයි කියා\nකුමරී\nසිහින දකින්න\nමගේ සිහිනේ හිමිකාරිය\nවන්න\nදිවියට එන්න\nපවසන් නුඹ මගෙමයි කියා\nසංසාර සිහිනයේ\nමා නුඹේ කුමරා\n\nසංසාර සිහිනේ\nපෙර මග මා සිටියා\nකවදාද නුඹ එන්නේ\nආදර දේදුන්නේ\nඅඩවන් නෙතු විහිදා\nමා නුඹේ කුමරා\n\nසංසාර සිහිනයේ\nමා නුඹේ කුමරා\nමා නුඹේ කුමරා\nමා නුඹේ කුමරා\nමා නුඹේ කුමරා',
+    type: 'Solo',
+  },
+  {
+    audio: {
+      audioPath: 'audios\\Landune_Landune.mp3',
+      audio: 'http://localhost:4000/Landune_Landune.mp3',
+      audioAvailability: true,
+    },
+    categories: ['Love'],
+    likes: 0,
+    artist: ['5e89b94169be1558f04ed070'],
+    _id: '5e89bbbc69be1558f04ed074',
+    sinhalaTitle: 'ළඳුනේ',
+    singlishTitle: 'Landune',
+    song:
+      'ළඳුනේ - ළඳුනේ\nඔබේ දෑස දැකගත්තෙමි\nසැඳෑ කලෙක ළඳුනේ - ළඳුනේ\nදෙනෝ දහක් අත ගත්තද\nසැමියෙකු නැති ළඳුනේ\nනෑයෙක් නෑකම නොකියන\nවීදි සරණ ළඳුනේ - ළඳුනේ\nතෙල් මල් ගෙන දෝත පුරා\nමුනිකුටියට පියමං වන\nඔබේ දෑස දැක ගත්තෙමි\nසැඳෑ කලෙක ළඳුනේ - ළඳුනේ\nබුදුන් දැක නිවන් දකිමැයි\nදෙව්රම යන පාර අහන\nඔබ ද පටාචාරාවකි\nඔබ ද කිසාගෝතමියකි\nපිරිමින්ගේ පාප කඳට\nදෝස විඳින ළඳුනේ\nඅඳුරේ පව් කරන දනා එළියෙ රවන ළඳුනේ\nළැම පමණක් ලොවට පෙනෙන\nළය නොපෙනෙන ළඳුනේ\nකුහුඹුවකුට වරදක් නැති\nවැරදිකාර ළඳුනේ',
+    type: 'Solo',
+  },
+  {
+    audio: {
+      audioPath: 'audios\\Hiru Mal_Hiru_Mal_Kiniththak_Dara.mp3',
+      audio: 'http://localhost:4000/Hiru Mal_Hiru_Mal_Kiniththak_Dara.mp3',
+      audioAvailability: true,
+    },
+    categories: ['Love', 'Novelty'],
+    likes: 0,
+    artist: ['5e89b8df69be1558f04ed06f', '5e89bb4e69be1558f04ed073'],
+    _id: '5e89bc5669be1558f04ed075',
+    sinhalaTitle: 'හිරු මල්',
+    singlishTitle: 'Hiru Mal',
+    song:
+      'හිරු මල් කිනිත්තක් දරා\nසිහිළැල් පියාපත් සලා\nසුළඟින් හොරෙන් මග අසා\nඑන්නම් සැනින් ඔබ සොයා\nමතකෙන් ඉඩක් ඉල්ලලා\nතුරුලෙන් ලැගුම් ගෙන එදා\nහසරැල් ලොවක් හිත මවා\nහෙට මා ඔයාගෙ වෙනවා\nහෙට මා ඔයාගෙ වෙනවා\nසුපෙම් භාවනාවක් වාගේ මගේ දෙස බලා\nකියාදෙන්න ඔබගේ සෙනෙහස කොතෙක් දැයි කියා\nඅහංකාර මාගේ හිත දැන් ඔබේ ළඟ තමා\nනොදන්නවා වාගේ ඈතින් තවත් ඉන්නෙපා\nහිතේ මැවුණ ප්රේම කවියේ හිඩැස් තැන් මකා\nහිඳින්නට හිතයි සුවසේ තුරුල් වී සදා\nරැයේ දුටුව හීන රහසේ හැබෑ වී මෙදා\nපුරුද්දක්ද මේ දිවියේ මටත් නොහැඟ=ණා\nපහන් තාරකාවක් මාගේ හදේ පායලා\nගිලන් පෙම් පැතුම් හිත පතුලේ කොහේටද වෙලා\nනෙතින් වැටුන කැදිලැල් ඔබගේ අතින් පිස දමා\nසිනාසෙන්න ආසයි හැමකල් ඔබේ ලොව පුරා\nපහන් ටැඹ වෙලා දිවිගමනේ නවාතැන් සොයා\nරැෙඳන්නට හිතයි සෙවනේ සුසුම් හැර දමා\nඔඛෙන් ලැබුණ ආදරේ නම් අනන්තෙට එහා\nපුරුද්දක්ද මේ දිවියේ මටත් නොහැඟ=නා\nහිරු මල් කිනිත්තක්...\nමතකෙන් ඉඩක් ඉල්ලලා...',
+    type: 'Duet',
+  },
+  {
+    audio: {
+      audioPath: 'audios\\Saragi Asille_Saragi_Asille_(Nethu_Oba_Ma).mp3',
+      audio:
+        'http://localhost:4000/Saragi Asille_Saragi_Asille_(Nethu_Oba_Ma).mp3',
+      audioAvailability: true,
+    },
+    categories: ['Love', 'Dance'],
+    likes: 0,
+    artist: [
+      '5e89b87769be1558f04ed06e',
+      '5e89b8df69be1558f04ed06f',
+      '5e89bac869be1558f04ed072',
+    ],
+    _id: '5e89bcb269be1558f04ed076',
+    sinhalaTitle: 'සරාගී ඇසිල්ලේ',
+    singlishTitle: 'Saragi Asille',
+    song:
+      'නෙතු ඔබ මා ගැටුණා ඒ ගංතීරේ\nඔබ මට ඉඟි කෙරුවා හැබැහින්දෝ නොදැනේ\nනෙතු ඔබ මා බැදුණා ඒ ගංතීරේ\nඔබ මට ඉගි කෙරුවා හැබැහින්දෝ නොදැනේ\nසිහිනෙන් පෑ වදනින්\nනෙතු කැල්මේ බැල්මෙන්\nතවමත් මා හද කැළඹේ\nසරාගී ඇසිල්ලේ පෙමින් මත් වීලා ඈගේ\nතුරුල් වී සමීපේ කොපුල් තෙත් වීලා රහසේ\nසරාගී ඇසිල්ලේ පෙමින් මත් වීලා ඈගේ\nතුරුල් වී සමීපේ කොපුල් තෙත් වීලා රහසේ\nදෙනුවන් සිප ගත්තා පිනි පොද වැස්සේ\nඅප හදවත් බැඳිලා මන්දාරම් සෙවණේ\nදෙනුවන් සිප ගත්තා පිනි පොද වැස්සේ\nඅප හදවත් බැඳිලා මන්දාරම් සෙවණේ\nසිහිනෙන් පෑ වදනින්\nනෙතු කැල්මේ බැල්මෙන්\nතවමත් මා හද කැළඹේ\nසරාගී ඇසිල්ලේ පෙමින් මත් වීලා ඈගේ\nතුරුල් වී සමීපේ කොපුල් තෙත් වීලා රහසේ\nසරාගී ඇසිල්ලේ පෙමින් මත් වීලා ඈගේ\nතුරුල් වී සමීපේ කොපුල් තෙත් වීලා රහසේ\nඈ දුටු විට දෙවඟනක් වගේ\nහදවත කකියනවා හොදටම මගේ\nදිලිසෙන නුවන් නිල්මිණි කැට වගේ\nඅකාසේ තරු මදි ගෙල සරසන්න ඇගේ\nඇගේ ඉඟ සුඟ ගත හැකි මගේ අත මිටින\nනිසි පුලුලුකුල මගෙ රිය සක ගැටුණ\nබෝනික්කියක් වගේ ඇසිපිය නොම හෙලන\nඈ දුටුවම වුණා මගේ හදවත සසල\nඔය දෙතොලඟ රැඳි සිනාවේ\nබැදුණා හදවතින් රහසේ\nපෙම් පහසක රැඳුණු මානේ\nමම අයෙත් ආලෙන් මත් වුණාවේ\nමිරිකුවා හදවත ඈ යන්තම්\nපත්තු වුනෙ හදවත මගේ ගිනි පන්දම්\nගැලපුන අපි දෙන්නගේ පොරොන්දම්\nගැලවෙන්න බෑ ඇලවිලා වගේ කාන්දම්\nඔය හසරැල් දුටුදා මා නෙත් මානේ\nපෙම් හසුනක් එව්වා මා හදවත රහසේ\nඔය හසරැල් දුටුදා මා නෙත් මානේ\nපෙම් හසුනක් එව්වා මා හදවත රහසේ\nසිහිනෙන් පෑ වදනින්\nනෙතු කැල්මේ බැල්මෙන්\nතවමත් මා හද කැළඹේ\nසරාගී ඇසිල්ලේ පෙමින් මත් වීලා ඈගේ\nතුරුල් වී සමීපේ කොපුල් තෙත් වීලා රහසේ\nසරාගී ඇසිල්ලේ පෙමින් මත් වීලා ඈගේ\nතුරුල් වී සමීපේ කොපුල් තෙත් වීලා රහසේ',
+    type: 'Group',
+  },
+];
